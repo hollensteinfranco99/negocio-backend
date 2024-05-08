f8c5d9c1da9534c734e0c6630b4989341dc712ee
Franco Hollenstein <hollensteinfranco99@gmail.com>
2024-05-08 05:27:41 -0300

    se modifica formato

--- src/database.js
+++ src/database.js
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+
+
 
 const url = 'mongodb+srv://hollensteinfranco99:Pucho2020.@francohollenstein99.gwr3wbe.mongodb.net/puntoDeVenta';
 //const url = 'mongodb://127.0.0.1:27017/KioskoDB';
-        mongoose.connect(url);
-        const connection = mongoose.connection;
+mongoose.connect(url);
+const connection = mongoose.connection;
 
-        connection.once('open', () => {
-            console.log("¡Conexión exitosa!");
-        });
+connection.once('open', () => {
+    console.log("¡Conexión exitosa!");
+});
 
-        connection.on('error', (err) => {
-            console.error("Error de conexión:", err);
-        });
-
-
+connection.on('error', (err) => {
+    console.error("Error de conexión:", err);
+});
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+/*import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
@@ -9,29 +9,43 @@ import compraPedidoRoutes from './routes/compraPedido.routes';
 import detalleCompraRoutes from './routes/detalleComprobanteCompra.routes';
 import caja from './routes/caja.routes';
 import detalleVentaRoutes from './routes/detalleComprobanteVenta.routes';
-import movimiento from './routes/movimiento.routes';
+import movimiento from './routes/movimiento.routes';*/
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+require('./database');
+const productoRoutes = require('./routes/producto.routes');
+const ventaRoutes = require('./routes/venta.routes');
+const compraPedidoRoutes = require('./routes/compraPedido.routes');
+const detalleCompraRoutes = require('./routes/detalleComprobanteCompra.routes');
+const caja = require('./routes/caja.routes');
+const detalleVentaRoutes = require('./routes/detalleComprobanteVenta.routes');
+const movimiento = require('./routes/movimiento.routes');
+
 
 // Configuracion
 // Crear instancia de express
 const app = express();
 
-app.set('port',process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4000);
 
-app.listen(app.get('port'),()=>{
-    console.log("Estoy en el puerto "+app.get('port'));
+app.listen(app.get('port'), () => {
+    console.log("Estoy en el puerto " + app.get('port'));
 });
 
 // Herramientas extras
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"../public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use('/api/puntoDeVenta',productoRoutes);
-app.use('/api/puntoDeVenta',ventaRoutes);
-app.use('/api/puntoDeVenta',compraPedidoRoutes);
-app.use('/api/puntoDeVenta',detalleCompraRoutes);
-app.use('/api/puntoDeVenta',detalleVentaRoutes);
-app.use('/api/puntoDeVenta',movimiento);
-app.use('/api/puntoDeVenta',caja);
+app.use('/api/puntoDeVenta', productoRoutes);
+app.use('/api/puntoDeVenta', ventaRoutes);
+app.use('/api/puntoDeVenta', compraPedidoRoutes);
+app.use('/api/puntoDeVenta', detalleCompraRoutes);
+app.use('/api/puntoDeVenta', detalleVentaRoutes);
+app.use('/api/puntoDeVenta', movimiento);
+app.use('/api/puntoDeVenta', caja);
